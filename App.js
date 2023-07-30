eb24d6e6e26e7d1ce2cffeec6db2ea59be834a11
tinalouwho <117543318+tinalouwho@users.noreply.github.com>
2023-07-30 16:42:33 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Appearance, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaProvider>
+    <View style={[styles.container, themeContainerStyle]}>
+      <Text style={[styles.title, themeTextStyle]}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  lightContainer: {
+    backgroundColor: '#f3f3f3',
+  },
+  darkContainer: {
+    backgroundColor: '#1b1b1b',
+  },
+  lightThemeText: {
+    color: '#1b1b1b',
+  },
+  darkThemeText: {
+    color: '#f3f3f3',
+  },
+ 
 });
